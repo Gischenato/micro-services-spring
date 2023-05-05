5c4da4f365e3743b59684d82cf886c3c65022502
Giovanni Schenato <gischenato@gmail.com>
2023-05-05 14:02:03 -0300

    Student Service

---
+++ student/src/main/java/com/gischenato/student/StudentService.java
@@ -0,0 +1,23 @@
+package com.gischenato.student;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+
+    private final StudentRepository repository;
+
+
+    public void saveStudent(Student student) {
+        repository.save(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return repository.findAll();
+    }
+}
