bc6570e0d300156bae507f794ab95e09288ddef3
Giovanni Schenato <gischenato@gmail.com>
2023-05-05 13:48:50 -0300

    initial Student service

---
+++ student/src/main/java/com/gischenato/student/Student.java
@@ -0,0 +1,19 @@
+package com.gischenato.student;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    private Integer id;
+    
+}
