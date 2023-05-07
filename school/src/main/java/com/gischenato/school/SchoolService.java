bf95e3f1bedbc3ef90e6a4794b6da44cf9523562
Gischenato <gischenato@gmail.com>
2023-05-07 01:05:32 -0300

    find school with students

--- school/src/main/java/com/gischenato/school/SchoolService.java
+++ school/src/main/java/com/gischenato/school/SchoolService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.gischenato.school.clients.StudentClient;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -11,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 public class SchoolService {
 
     private final SchoolRepository repository;
+    private final StudentClient client;
 
 
     public void saveStudent(School student) {
@@ -20,4 +23,22 @@ public class SchoolService {
     public List<School> getAllStudents() {
         return repository.findAll();
     }
+
+    public FullSchoolResponse findSchoolsWithStudents(Integer schoolId) {
+        var school = repository.findById(schoolId)
+                                .orElse(
+                                    School.builder()
+                                    .name("NOT_FOUND")
+                                    .email("NOT_FOUND")
+                                    .build()
+                                );
+
+        var students = client.findAllStudentsBySchool(schoolId);
+
+        return FullSchoolResponse.builder()
+        .name(school.getName())
+        .email(school.getEmail())
+        .students(students)
+        .build();
+    }    
 }
